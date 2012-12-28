9fc02e74f32d34c86a076db98360249409be4d5f
Josh Adams <josh@isotope11.com>
2012-12-28 16:47:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsotopeWiki::Application.config.secret_token = '3a25bb1da4cc0e7d712b76368673485abcabee7d6f58fbb43d20cb5f31963e74a561fa992667896d92e38b1fa8bc5b3cb80b3e467b8981536b2096bab38e428e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IsotopeWiki::Application.config.session_store :cookie_store, key: '_isotope-wiki_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IsotopeWiki::Application.config.session_store :active_record_store
