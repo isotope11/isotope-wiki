c18524a4f681731cd1d04ce7def694f2f9af575a
Josh Adams <josh@isotope11.com>
2013-01-25 18:55:14 -0600

    Remove vim mode, update wacky

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,8 +1,8 @@
 $(function(){
   if($('#page_editor')[0]){
     var editor = ace.edit("page_editor");
-    require(['ace/keyboard/vim'], function(vimMode){
-      editor.setKeyboardHandler(vimMode.handler);
-    });
+    //require(['ace/keyboard/vim'], function(vimMode){
+      //editor.setKeyboardHandler(vimMode.handler);
+    //});
   }
 });
